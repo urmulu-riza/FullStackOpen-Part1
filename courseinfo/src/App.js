ad80064cfba2d7552e4fe1b2a73ced8824c5fba1
mr1mokhtari <mr1mokhtari@gmail.com>
2023-05-28 18:51:02 +0430

    1.1: Courseinfo Exercise, Step1 - shorter props in content

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -34,12 +34,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        {...{ part1, part2, part3, exercises1, exercises2, exercises3 }}
       />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
